b8f4f1b9592b46e5654fbe0a7f801b48928895b8
HeloaGuizzo <107003060+HeloaGuizzo@users.noreply.github.com>
2022-06-13 17:59:25 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 2 Exercício 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um Número");
+            int valor1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um Número");
+            int valor2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um Número");
+            int valor3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um Número");
+            int valor4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um Número");
+            int valor5 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um Número");
+            int valor6 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um Número");
+            int valor7 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um Número");
+            int valor8 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um Número");
+            int valor9 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um Número");
+            int valor10 = Convert.ToInt32(Console.ReadLine());
+
+            int valorTotal = valor1 + valor2 + valor3 + valor4 + valor5 + valor6 + valor7 + valor8 +
+                valor9 + valor10;
+
+            Console.WriteLine("A soma dos números é: " + (valorTotal));
+        }
+    }
+}
